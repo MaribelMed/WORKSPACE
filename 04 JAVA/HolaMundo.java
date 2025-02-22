505d823450cd4337627b74eb8a7079afe2a0404f
NaribelMed <maribelmed@gmail.com>
2025-02-22 16:18:20 +0100

    cursojava_v.1

--- 04 JAVA/HolaMundo.java
+++ 04 JAVA/HolaMundo.java
@@ -5,7 +5,7 @@ class HolaMundo {
     // Declaración del método 'main' el cual contiene un vector de tipo string
     public static void main(String[] args) {
         // Instrucción que imprime or pantalla una cadena de texto
-        // las instrucciones que incluyen funciones se separan con punto y coma.
+        // las instrucciones que incluyen funciones y se separan con punto y coma.
         System.out.println("Hola Mundo");
     }
 }
