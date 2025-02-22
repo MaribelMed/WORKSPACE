e866f6c78bbfa745c8be048f384f985f57993c2d
NaribelMed <maribelmed@gmail.com>
2025-02-22 16:10:04 +0100

    curso java v.0

---
+++ 04 JAVA/HolaMundo.java
@@ -0,0 +1,11 @@
+
+// Aplicación Hola Mundo (esto es un comentario y sirve para mejorar la lectura del codigo, no se ejecuta)
+//declaración de la clase HolaMundo
+class HolaMundo {
+    // Declaración del método 'main' el cual contiene un vector de tipo string
+    public static void main(String[] args) {
+        // Instrucción que imprime or pantalla una cadena de texto
+        // las instrucciones que incluyen funciones se separan con punto y coma.
+        System.out.println("Hola Mundo");
+    }
+}
